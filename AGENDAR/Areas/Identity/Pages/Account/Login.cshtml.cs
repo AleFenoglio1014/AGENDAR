bc008e965e3ab062a738ce30de37c41d6b4c7136
Gonzi Pagliano <gonza.pagliano@gmail.com>
2022-06-15 22:06:33 -0300

    Login

--- AGENDAR/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ AGENDAR/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -73,7 +73,7 @@ namespace AGENDAR.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl = returnUrl ?? Url.Content("~/");
+            returnUrl = returnUrl ?? Url.Content("~/Empresas/Index");
 
             if (ModelState.IsValid)
             {
