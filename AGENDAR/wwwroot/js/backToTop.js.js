1e619cc6ace2afcf9142a8f31b22ab8356bc8479
valentin beletti <v-a-l-e_7@hotmail.com>
2022-07-04 17:43:23 -0300

    Boton para volver hacia arriba

---
+++ AGENDAR/wwwroot/js/backToTop.js.js
@@ -0,0 +1,16 @@
+﻿var botonTop = document.getElementById('top1');
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        botonTop.style.display = "block";
+    } else {
+        botonTop.style.display = "none";
+    }
+}
+
+function top1() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
