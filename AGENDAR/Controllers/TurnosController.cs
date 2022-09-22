53e1fe109e3b00c090c1d2bb58ab7f44deb6492a
valentin beletti <v-a-l-e_7@hotmail.com>
2022-09-22 13:06:46 -0300

    titulos en español, calendario dia actual, filtrado por empresa

--- AGENDAR/Controllers/TurnosController.cs
+++ AGENDAR/Controllers/TurnosController.cs
@@ -23,11 +23,13 @@ namespace AGENDAR.Controllers
         }
 
         //Funcion para Buscar Empresa y Usuario
+      
         public void BuscarEmpresaActual(string usuarioActual, EmpresaUsuario empresaUsuarioActual)
         {
-            empresaUsuarioActual = _context.EmpresasUsuarios.Where(p => p.UsuarioID == usuarioActual).SingleOrDefault();
-        }
+            var empresalogueada = _context.EmpresasUsuarios.Where(p => p.UsuarioID == usuarioActual).SingleOrDefault();
+            empresaUsuarioActual.EmpresaID = empresalogueada.EmpresaID;
 
+        }
         public IActionResult IndexHomePublico()
         {
             var provincias = _context.Provincia.ToList();
@@ -65,44 +67,6 @@ namespace AGENDAR.Controllers
             return View();
 
         }
-
-
-
-        public JsonResult BuscarTurnos()
-        {
-            var turnos = _context.Turnos.Include(r => r.Horarios).Include(r => r.Horarios.Profesionales).Include(r => r.Horarios.Profesionales.Empresas).Include(r => r.Horarios.Profesionales.Empresas.Localidades).Include(r => r.Horarios.Profesionales.Empresas.Localidades.Provincias).ToList();
-
-            List<TurnoMostrar> listadoTurnoMostrar = new List<TurnoMostrar>();
-            foreach (var turno in turnos)
-            {
-                var turnoMostrar = new TurnoMostrar()
-                {
-                    TurnoID = turno.TurnoID,
-                    Nombre = turno.Nombre,
-                    Apellido = turno.Apellido,
-                    Telefono = turno.Telefono,
-                    Email = turno.Email,
-                    HorarioID = turno.HorarioID,
-                    HorarioFecha = turno.Horarios.HorarioCompleto,
-                    ProfesionalID = turno.Horarios.Profesionales.ProfesionalID,
-                    ProfesionalNombre = turno.Horarios.Profesionales.ProfesionalNombreCompleto,
-                    EmpresaID = turno.Horarios.Profesionales.Empresas.EmpresaID,
-                    EmpresaNombre = turno.Horarios.Profesionales.Empresas.RazonSocial,
-                    LocalidadID = turno.Horarios.Profesionales.Empresas.Localidades.LocalidadID,
-                    LocalidadNombre = turno.Horarios.Profesionales.Empresas.Localidades.Descripcion,
-                    ProvinciaID = turno.Horarios.Profesionales.Empresas.Localidades.Provincias.ProvinciaID,
-                    ProvinciaNombre = turno.Horarios.Profesionales.Empresas.Localidades.Provincias.Descripcion,
-                    Elimindado = turno.Eliminado
-                    
-
-                };
-                listadoTurnoMostrar.Add(turnoMostrar);
-            }
-
-            return Json(listadoTurnoMostrar);
-        }
-
-
 
         //CODIGO PARA GUARDAR EL TURNO
         public JsonResult GuardarTurno(int TurnoID, string Nombre, string Apellido, string Email, Int64 Telefono,DateTime FechaTurno, int LocalidadID, int EmpresaID, int ProfesionalID, int ProvinciaID, int HorarioID/*, string HorarioFecha*/)
@@ -163,7 +127,11 @@ namespace AGENDAR.Controllers
         //Función para para mostrar los turnos en el Calendario del Profesional
         public JsonResult MostrarTurnosInterno()
         {
-            var turnosCalendario = _context.Turnos.Include(r => r.Horarios).Include(r => r.Horarios.Profesionales).Include(r => r.Horarios.Profesionales.Empresas).Include(r => r.Horarios.Profesionales.Empresas.Localidades).Include(r => r.Horarios.Profesionales.Empresas.Localidades.Provincias).ToList();
+            var usuarioActual = _userManager.GetUserId(HttpContext.User);
+            //LUEGO EN BASE A ESE USUARIO BUSCAMOS LA EMPRESA CON LA QUE ESTA RELACIONADA
+            EmpresaUsuario empresaUsuarioActual = new EmpresaUsuario();
+            BuscarEmpresaActual(usuarioActual, empresaUsuarioActual);
+            var turnosCalendario = _context.Turnos.Include(r => r.Horarios).Where(l => l.EmpresaID == empresaUsuarioActual.EmpresaID).ToList();
             List<TurnoMostrar> listadoTurnosCalendario = new List<TurnoMostrar>();
             foreach(var turno in turnosCalendario)
             {
